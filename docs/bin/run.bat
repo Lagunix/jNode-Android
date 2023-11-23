78352a0d8756df7a5fc9e4964f4fb2a139a515c2
Lagunix <69526995+Lagunix@users.noreply.github.com>
2023-11-23 12:08:52 +0300

    Delete docs/bin/run.bat

--- docs/bin/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setLocal EnableDelayedExpansion
-set CLASSPATH=.
-for /R ../lib %%a in (*.jar) do (
-   set CLASSPATH=!CLASSPATH!;"%%a"
-)
-set CLASSPATH=!CLASSPATH!
-echo !CLASSPATH!
-java -Xmx300m -server -cp !CLASSPATH! jnode.main.Main ..\\etc\\jnode.win.conf
