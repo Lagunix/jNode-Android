61c9bb081cf84d2c29c4845b3fbe02d76d1d7990
Lagunix <69526995+Lagunix@users.noreply.github.com>
2023-12-18 12:46:13 +0300

    Update BinkpIPv6Connector.java

--- jnode-core/src/jnode/protocol/binkp/connector/BinkpIPv6Connector.java
+++ jnode-core/src/jnode/protocol/binkp/connector/BinkpIPv6Connector.java
@@ -1,13 +1,19 @@
 /*
 JVM_ARGS
 -Djava.net.preferIPv4Stack=false -Djava.net.preferIPv6Addresses=true
+
+sun.net.spi.nameservice.provider.<n>=<default|dns,sun|...>
+sun.net.spi.nameservice.nameservers=<server1_ipaddr,server2_ipaddr ...>
+
+import sun.net.util.IPAddressUtil.isIPv6LiteralAddress
+public static boolean isIPv6Format(String ip {
+ip=ip.trim();
 */
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
-
+ 
 import java.net.Inet6Address
 import java.net.InetSocketAddress;
 import java.net.Socket;
