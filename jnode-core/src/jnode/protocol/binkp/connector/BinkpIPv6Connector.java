4cfb09ff1ea817716e054c10629f97b3b7f3a831
Lagunix <69526995+Lagunix@users.noreply.github.com>
2023-12-18 11:23:26 +0300

    Update connector

---
+++ jnode-core/src/jnode/protocol/binkp/connector/BinkpIPv6Connector.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+
+import java.util.NoSuchElementException;
+import java.util.ArrayList;
+import java.util.List;
